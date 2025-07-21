6cb92980a8e0e2dd2557d9b14b5ccd9ffb622557
Alex Gar <ale.ag538@gmail.com>
2025-07-21 15:11:18 -0600

    first commit

---
+++ src/test/java/com/topicos/topicos/TopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos.topicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
